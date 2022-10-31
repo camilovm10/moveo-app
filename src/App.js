32a488deebcb66d1cded889cce2742711abbea4b
juan vasquez <camilovasquez9738@hotmail.com>
2022-10-31 17:09:49 -0500

    CSS basic

--- src/App.js
+++ src/App.js
@@ -115,89 +115,93 @@ const App = () => {
   }
 
   return (
-    <div className="destinatariosDisponiblesContainer">
+    <div className="mensajeria_app">
 
       <h1>EVERGREEN MENSAJERIA</h1>
 
-      <div>
-        <h2>Destinatarios disponibles:</h2>
-        {destinatariosData?.length > 0 ? (
-        <div>
-          <ul>
-          {destinatariosData.map((dest) => (
-            <li>
-              <h3>{`Destinatario ${dest.id}`}</h3>
-              <p>{dest.nombre}</p>
-              <p>{dest.numero}</p>
-              <p>{dest.correo}</p>
-            </li>
-          ))}
-          </ul>
-        </div>
-        ) : (
+      <div className='disponibles_container'>
+        <div className="destinatarios_disponibles_container disp_container">
+          <h2>Destinatarios disponibles:</h2>
+          {destinatariosData?.length > 0 ? (
           <div>
-            <h3>No hay destinatarios disponibles</h3>
+            <ul>
+            {destinatariosData.map((dest) => (
+              <li>
+                <h3>{`Destinatario ${dest.id}`}</h3>
+                <p>{dest.nombre}</p>
+                <p>{dest.numero}</p>
+                <p>{dest.correo}</p>
+              </li>
+            ))}
+            </ul>
           </div>
-        )}
-      </div>
-
-      <div>
-        <h2>Remitentes disponibles:</h2>
-        {remitentesData?.length > 0 ? (
-        <div>
-          <ul>
-          {remitentesData.map((rem) => (
-            <li>
-              <h3>{`Remitente ${rem.id}`}</h3>
-              <p>{rem.nombre}</p>
-              <p>{rem.numero}</p>
-              <p>{rem.correo}</p>
-            </li>
-          ))}
-          </ul>
+          ) : (
+            <div>
+              <h3>No hay destinatarios disponibles</h3>
+            </div>
+          )}
         </div>
-        ) : (
-          <div>
-            <h3>No hay remitentes disponibles</h3>
-          </div>
-        )}
-      </div>
 
-      <div>
+        <div className="remitentes_disponibles_container disp_container">
+          <h2>Remitentes disponibles:</h2>
+          {remitentesData?.length > 0 ? (
+          <div>
+            <ul>
+            {remitentesData.map((rem) => (
+              <li>
+                <h3>{`Remitente ${rem.id}`}</h3>
+                <p>{rem.nombre}</p>
+                <p>{rem.numero}</p>
+                <p>{rem.correo}</p>
+              </li>
+            ))}
+            </ul>
+          </div>
+          ) : (
+            <div>
+              <h3>No hay remitentes disponibles</h3>
+            </div>
+          )}
+        </div>
+      </div>
+      
+
+      <div className="detallar_container">
         <h1>Detallar mensaje :</h1>
-      </div>
+      
+        <div className='message_form'>
 
-      <div className='message_form'>
+          <label className='message_form_label'>Id Destinatario:</label>
+          <input 
+            placeholder='destinatario...'
+            value={mensajeForm.idDestinatario}
+            onChange={(e) => setDestinatario(e.target.value)}
+          />
 
-        <label>Id Destinatario:</label>
-        <input 
-          placeholder='destinatario...'
-          value={mensajeForm.idDestinatario}
-          onChange={(e) => setDestinatario(e.target.value)}
-        />
+          <label className='message_form_label'>Asunto:</label>
+          <input 
+            placeholder='asunto...'
+            value={mensajeForm.asunto}
+            onChange={(e) => setAsunto(e.target.value)}
+          />
 
-        <label>Asunto:</label>
-        <input 
-          placeholder='asunto...'
-          value={mensajeForm.asunto}
-          onChange={(e) => setAsunto(e.target.value)}
-        />
+          <label className='message_form_label'>Mensaje:</label>
+          <input 
+            placeholder='mensaje...'
+            value={mensajeForm.cuerpo}
+            onChange={(e) => setCuerpo(e.target.value)}
+          />
 
-        <label>Mensaje:</label>
-        <input 
-          placeholder='mensaje...'
-          value={mensajeForm.cuerpo}
-          onChange={(e) => setCuerpo(e.target.value)}
-        />
-
-        <label>Id Remitente:</label>
-        <input 
-          placeholder='remitente...'
-          value={mensajeForm.idRemitente}
-          onChange={(e) => setIdRemitente(e.target.value)}
-        />
+          <label className='message_form_label'>Id Remitente:</label>
+          <input 
+            placeholder='remitente...'
+            value={mensajeForm.idRemitente}
+            onChange={(e) => setIdRemitente(e.target.value)}
+          />
+        </div>
 
         <button
+          className='btn'
           type="button"
           onClick={(e) => {
             setEstado(1)
